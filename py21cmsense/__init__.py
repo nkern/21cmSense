504285a688cadce3adf37170931cb3b6b5fe2bfd
Nicholas S. Kern <nkern@berkeley.edu>
2016-11-25 08:51:14 -0800

    modified:   py21cmsense/__init__.py

--- py21cmsense/__init__.py
+++ py21cmsense/__init__.py
@@ -1,1 +1,8 @@
+"""
+py21cmsense
+
+Python code for calculating the sensitivity of an interferometer
+to the 21cm Power Spectrum at the Epoch of Reionization
+"""
 from .utils import (load_noise_files,noise_interp2d)
+from .calc_sense import *
